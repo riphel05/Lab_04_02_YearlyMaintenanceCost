8b976829ef056fcb4a923d65322fc56619ba95c5
riphel05 <phelpsre@mail.uc.edu>
2022-09-18 19:52:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class
+Main {
+    public static void main(String[] args)
+    {
+
+    double winterMaintenance = 450.67;
+    double springMaintenance = 200.87;
+    double summerMaintenance = 321.98;
+    double fallMaintenance = 287.08;
+
+        double yearlyMaintenance = winterMaintenance + springMaintenance + summerMaintenance + fallMaintenance;
+            System.out.println("With the Winter cost of " + winterMaintenance + ", spring cost of " + springMaintenance + ", summer cost of " + summerMaintenance + ", and the fall cost of " + fallMaintenance + ", the total yearly maintenance cost comes to " + yearlyMaintenance);
+
+
+    }
+}
